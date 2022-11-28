0507a4fdb4a5170d55c2c050db029a1737c10076
Rodrigo Castro <rodrigocastro101998@gmail.com>
2022-11-28 15:36:16 -0300

    agrega manifest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,113 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+        }
 
-                        <div class="mt-3">
-                            <h3>Todos los post</h3>
-                            <div class="container">
-                                <div class="search">
-                                    <input type="search" name="search" id="search" placeholder="Buscar"
-                                        class="form-control">
+        body {
+            background: #fefefe;
+            font-family: sans-serif;
 
-                                </div>
-                            </div>
-                            <br>
-                            <div id="Content" class="searchdata">
-                            </div>
-                            <div class="alldata">
-                                @forelse ($posts as $post)
-                                    <div class="card" style="display:inline-block">
-                                        <td>Nombre: {{ $post->name }}</td>
-                                        <br>
-                                        <td>Categoria: {{ $post->categoria->name }}</td>
-                                        </tr>
-                                        <div class="d-flex">
-                                            <img src="{{ asset($post->foto) }}" alt="image" width="200px">
-                                        </div>
-                                        <br>
+        }
 
-                                    @empty
-                                        <li class="list-group-item">Sin posts</li>
-                                @endforelse
-                            </div>
-                            </tr>
+        .container {
+
+        }
+
+        .heading {
+            text-align: center;
+            font-size: 30px;
+            margin-bottom: 5px;
+        }
+
+        .row {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+        }
+
+        .cardpost {
+            width: 30%;
+            background: rgb(240, 236, 236);
+            border: 1px solid rgb(4, 4, 4);
+            margin-bottom: 10px;
+            transition: 0.3s;
+        }
+
+        .heading-card {
+            text-align: center;
+            padding: 50px 10px;
+            background: linear-gradient(to right, #0f0f0f, #a5918d);
+            color: #fff;
+        }
+
+        .card-bodypost {
+            padding: 30px 20px;
+            text-align: center;
+            font-size: 18px;
+        }
+
+        .cardpost:hover {
+            transform: scale(1.05);
+            box-shadow: 0 0 40px -10px rgba(0, 0, 0, 0.25);
+        }
+
+
+    </style>
+    <div class="row justify-content-center">
+        <div class="col-md-">
+            <div class="card">
+                <div class="card-header heading">
+                    <h3>Front page</h3>
+                </div>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
                         </div>
-
-
-
+                    @endif
+                    <div class="container">
+                        <div class="search">
+                            <input type="search" name="search" id="search" placeholder="Buscar" class="form-control">
+                        </div>
                     </div>
+
+                    <div id="Content" class="searchdata"></div>
+                    <br>
+
+                        <div class="alldata">
+                            <div class="row">
+                                @forelse ($posts as $post)
+
+                                <div class="cardpost">
+                                    <div class="heading-card">
+                                        <td>{{ $post->name }}</td>
+                                    </div>
+                                    <div class="headingcategoria-card">
+                                        <td>Categoria: {{ $post->categoria->name }}</td>
+                                    </div>
+                                    <div class="card-bodypost">
+                                        <p>Agregar descripcion</p>
+                                    </div>
+                                    <div class="">
+                                        <img src="{{ asset($post->foto) }}" alt="image" width="200px">
+                                    </div>
+                                </div>
+
+                                @empty
+                                    <li class="list-group-item">Sin posts</li>
+
+                            </div>
+                        </div>
+                    @endforelse
                 </div>
             </div>
         </div>
-    </div>
     </div>
 
     <script type="text/javascript">
@@ -60,9 +116,7 @@
             if ($value) {
                 $('.alldata').hide();
                 $('.searchdata').show();
-            }
-            else
-            {
+            } else {
                 $('.alldata').show();
                 $('.searchdata').hide();
             }
